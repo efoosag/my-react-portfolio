cd8db89665715689c76e9b993cfaafa4528be6e5
efoosag <efoosagsite@gmail.com>
2022-12-22 14:14:25 +0100

    add components and style file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About'
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Portfolio from './components/portfolio/Portfolio';
+import Services from './components/services/Services';
+import Testimonials from './components/testimonials/Testimonials';
 
 function App() {
   return (
-   <div>App</div>   
+   <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Services />
+    <Portfolio />
+    <Testimonials />
+    <Footer />
+   </>   
   );
 }
 
