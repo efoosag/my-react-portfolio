90269c387bc44b0414b027409a6acbea784dbd40
efoosag <efoosagsite@gmail.com>
2022-12-23 13:48:56 +0100

    add about component and style

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,39 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
 import './about.css'
+import aboutImg from '../../assets/aboutImg.png'
+import {FiAward} from 'react-icons/fi'
+import {AiFillFolderOpen} from 'react-icons/ai'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Know More</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={aboutImg} alt='About Me Image'/>
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FiAward className='about__card-icon'/>
+              <h5>Experience</h5>
+              <small>2+ years</small>
+            </article>
+            <article className="about__card">
+              <AiFillFolderOpen className='about__card-icon'/>
+              <h5>Projects</h5>
+              <small>30+ Completed Project</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aperiam animi voluptatibus iure alias. Praesentium dolore perspiciatis, accusamus eos, beatae, corporis maxime cumque repellendus quo magnam omnis deserunt aut architecto.</p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
