c61aa8f136e50cf0a8344d5643772df7c8de1b0b
Victor Efosa Osagie <efoosagsite@gmail.com>
2023-01-25 10:32:32 +0100

    Update Portfolio.jsx

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import './portfolio.css';
-import portf1 from '../../assets/portf1.png';
+import portf5 from '../../assets/portf5.png';
 import portf2 from '../../assets/portf2.png';
+import portf3 from '../../assets/portf3.png';
+import portf4 from '../../assets/portf4.png';
 
 const portfolio = [{
-  id: 1, title: 'Car Rentals App', address: 'https://github.com/efoosag/car-rental-app.git', img: portf1,
-},
-{
   id: 2, title: 'Space Travel', address: 'https://github.com/efoosag/space-travel-hub.git', img: portf2,
 },
 {
-  id: 3, title: 'Car Rentals App', address: 'https://github.com/efoosag/car-rental-app.git', img: portf1,
+  id: 3, title: 'Movies App', address: 'https://github.com/efoosag/movies-app.git', img: portf3,
 },
 {
-  id: 4, title: 'Space Travel', address: 'https://github.com/efoosag/space-travel-hub.git', img: portf2,
+  id: 4, title: 'Calculator App', address: 'https://github.com/efoosag/maths-magician.git', img: portf4,
 },
 {
-  id: 5, title: 'Car Rentals App', address: 'https://github.com/efoosag/car-rental-app.git', img: portf1,
-},
-{
-  id: 6, title: 'Space Travel', address: 'https://github.com/efoosag/space-travel-hub.git', img: portf2,
+  id: 5, title: 'Car Rentals App', address: 'https://github.com/efoosag/car-rental-app.git', img: portf5,
 },
 ];
 
